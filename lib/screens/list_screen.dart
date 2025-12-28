cd07d4b912d01cd3e574e06109259dc2568c2867
mel15g <fhb232585@fh-vie.ac.at>
2025-12-28 17:05:01 +0100

    Grundgerüst der App mit erstem Listen-Screen erstellt

---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Meine Einkaufsliste',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+
+
